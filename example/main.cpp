a0d17906be1fa8ec6f810a6ed7f4421fc2021f49
Owen Smith <ojman101@gmail.com>
2020-05-07 15:06:19 +0100

    [example] Replace with thread pool overhead test

--- example/main.cpp
+++ example/main.cpp
@@ -3,34 +3,41 @@
 #include <spdlog/spdlog.h>
 
 #include <chrono>
-#include <future>
-#include <random>
 #include <vector>
 
 using inexor::ecs::ThreadPool;
 
+namespace {
+
+constexpr int NUM_RUNS = 5;
+constexpr int NUM_JOBS = 1000000;
+
+} // namespace
+
 int main() {
-    spdlog::set_level(spdlog::level::trace);
+    spdlog::set_level(spdlog::level::info);
 
-    std::random_device rd;
-    std::mt19937 rng(rd());
-    std::uniform_int_distribution<> range(2, 8);
     ThreadPool pool;
+    std::vector<std::chrono::milliseconds::rep> results;
 
-    auto res = pool.submit([&]() {
-        std::vector<std::future<void>> futures;
+    spdlog::info("Thread pool overhead test");
+    spdlog::info("Peforming {} runs of submitting {} jobs", NUM_RUNS, NUM_JOBS);
 
-        for (int i = 0; i < 10; i++) {
-            futures.push_back(pool.submit([&]() {
-                std::this_thread::sleep_for(std::chrono::seconds(range(rng)));
-                spdlog::info("Hello world!");
-            }));
+    for (int i = 0; i < NUM_RUNS; i++) {
+        auto start = std::chrono::system_clock::now();
+        for (int j = 0; j < NUM_JOBS; j++) {
+            pool.submit([]() {});
         }
+        auto end = std::chrono::system_clock::now();
 
-        for (const auto &future : futures) {
-            future.wait();
-        }
-    });
+        results.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
+    }
 
-    while (res.wait_for(std::chrono::seconds(0)) != std::future_status::ready) {}
+    decltype(results)::value_type avg = 0;
+    for (int i = 0; i < results.size(); i++) {
+        spdlog::info("{} - {}", i, results[i]);
+        avg += results[i];
+    }
+
+    spdlog::info("Avg - {}", avg / results.size());
 }
